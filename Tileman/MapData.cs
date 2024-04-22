8b5646e0c64f40ccb4a11daa8ea44825a2842610
Freek <Freek@DESKTOP-8GESIS2>
2024-04-22 16:55:02 +0200

    Renamed project and namespace to Redux

--- Tileman/MapData.cs
+++ Tileman/MapData.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace Tileman
+namespace TilemanRedux
 {
-    class MapData
+	class MapData
 
-    {
-        public List<KaiTile> AllKaiTilesList { get; set; } = new();
-    }
+	{
+		public List<KaiTile> AllKaiTilesList { get; set; } = new();
+	}
 }
--- Tileman/ModData.cs
+++ Tileman/ModData.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tileman
+﻿namespace TilemanRedux
 {
-    class ModData
+	class ModData
 
-    {
-        public bool ToPlaceTiles { get; set; }
-        public bool DoCollision { get; set; }  
-        
-        public bool AllowPlayerPlacement { get; set; }
-        public bool ToggleOverlay { get; set; }
+	{
+		public bool ToPlaceTiles { get; set; }
+		public bool DoCollision { get; set; }
 
-        public double TilePrice { get; set; }
-        public double TilePriceRaise { get; set; }
+		public bool AllowPlayerPlacement { get; set; }
+		public bool ToggleOverlay { get; set; }
 
-        public int CavernsExtra { get; set; }
-        public int DifficultyMode { get; set; } 
+		public double TilePrice { get; set; }
+		public double TilePriceRaise { get; set; }
 
-        public int PurchaseCount { get; set; }
+		public int CavernsExtra { get; set; }
+		public int DifficultyMode { get; set; }
 
-    }
+		public int PurchaseCount { get; set; }
+
+	}
 }
