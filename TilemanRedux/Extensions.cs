d5ed3812bc83bb24b97f82c4822aa7a0944deb4a
Freek <Freek@DESKTOP-8GESIS2>
2024-04-22 17:05:29 +0200

    Added extensions methods to preserve the old isTileLocationTotallyClearAndPlaceable method until the replacement methods have been tested

---
+++ TilemanRedux/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Objects;
+using xTile.Dimensions;
+
+namespace TilemanRedux;
+internal static class Extensions
+{
+	public static bool isTileLocationTotallyClearAndPlaceable(this GameLocation location, int x, int y)
+	{
+		return location.isTileLocationTotallyClearAndPlaceable(new Vector2(x, y));
+	}
+
+	public static bool isTileLocationTotallyClearAndPlaceable(this GameLocation location, Vector2 v)
+	{
+		Vector2 pixel = new Vector2((v.X * Game1.tileSize) + Game1.tileSize / 2, (v.Y * Game1.tileSize) + Game1.tileSize / 2);
+		foreach (Furniture f in location.furniture)
+		{
+			if (f.furniture_type != Furniture.rug && !f.isPassable() && f.GetBoundingBox().Contains((int)pixel.X, (int)pixel.Y) && !f.AllowPlacementOnThisTile((int)v.X, (int)v.Y))
+				return false;
+		}
+
+		return location.isTileOnMap(v) && !location.isTileOccupied(v) && location.isTilePassable(new Location((int)v.X, (int)v.Y), Game1.viewport) && location.isTilePlaceable(v);
+	}
+
+	public static bool isTileOccupied(this GameLocation location, Vector2 tileLocation, string characterToIgnore = "", bool ignoreAllCharacters = false)
+	{
+		CollisionMask mask = ignoreAllCharacters ? CollisionMask.All & ~CollisionMask.Characters & ~CollisionMask.Farmers : CollisionMask.All;
+		return location.IsTileOccupiedBy(tileLocation, mask);
+	}
+}
